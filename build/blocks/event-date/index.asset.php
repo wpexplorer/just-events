b660de237e207fa4e669c987fa0eaa339bae9e81
WPExplorer <wpexplorer@gmail.com>
2023-12-15 13:19:07 -0800

    Re-submit commit

--- build/blocks/event-date/index.asset.php
+++ build/blocks/event-date/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '78bc2bda25d2f7092763');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '01e51b2530252cac73e5');
--- build/blocks/event-link/index.asset.php
+++ build/blocks/event-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2632cb74110df4a4a9b6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '548ab9b92eafa85008c7');
