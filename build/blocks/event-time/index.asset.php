1a77749918c985542e10705e61fca6343a9071be
WPExplorer <wpexplorer@gmail.com>
2024-10-16 19:20:48 -0700

    Stop tracking build folder

--- build/blocks/event-time/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e1ba019c30f4c94ffbed');
--- build/custom-fields.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '99fb1d15d37b57ccbad4');
